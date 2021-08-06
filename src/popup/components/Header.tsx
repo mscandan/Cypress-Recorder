c34025628704f567a5afa22431ecca99f5b8b95a
M. Selcuk Candan <mehmetselcukcandan@icloud.com>
2021-08-06 16:10:05 +0300

    add button colors

--- src/popup/components/Header.tsx
+++ src/popup/components/Header.tsx
@@ -8,7 +8,7 @@ export interface HeaderProps {
 
 export default ({ shouldInfoDisplay, toggleInfoDisplay }: HeaderProps) => (
   <div id="header">
-    <h1 id="title">VirtualMetric</h1>
+    <h1 id="title">VirtualMetric Web Recorder</h1>
     <InfoButton shouldInfoDisplay={shouldInfoDisplay} toggleInfoDisplay={toggleInfoDisplay} />
   </div>
 );
